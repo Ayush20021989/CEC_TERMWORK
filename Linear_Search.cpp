8ef2def3b0d00c85b7d4a55a0e505673f749e061
Ayush20021989 <100331783+Ayush20021989@users.noreply.github.com>
2023-01-10 19:37:26 +0530

    Update Linear_Search.cpp

--- Linear_Search.cpp
+++ Linear_Search.cpp
@@ -1,4 +1,4 @@
-//Linear Search
+//Linear Search iterative approach
 #include<iostream>
 using namespace std;
 int Linear_Search(int arr[],int n,int key)
@@ -24,5 +24,38 @@ int main()
     cin>>key;
     int ans=Linear_Search(arr,n,key);
     (ans!=-1)?printf("Elements found at index:%d",ans):printf("Element not Found");
+    return 0;
+}
+
+//Linear Search Recursive approach
+#include<iostream>
+using namespace std;
+int Linear_Search(int arr[],int n,int key)
+{
+    if(n==0)
+    return -1;
+    if(arr[n-1]==key)
+    return n-1;
+    else
+    {
+   int ans=Linear_Search(arr,n-1,key);
+   return ans;
+    }
+   
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int key;
+    cout<<"Enter key value:";
+    cin>>key;
+    int ans=Linear_Search(arr,n,key);
+    (ans!=-1)?printf("Key found at %d position",ans):printf("Key not Found");
     return 0;
 }
